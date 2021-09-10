9e9f2e2c68a6a9d9d39266f6ee3fd6fa32b0dd8e
Shyam Kumar <shyamkumar@berkeley.edu>
2021-09-09 23:27:15 -0700

    Initial Commit

---
+++ Plates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Plates
+//
+//  Created by Shyam Kumar on 9/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
